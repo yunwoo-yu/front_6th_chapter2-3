f4128fc17f999cc2722c1483d989f6b0d9a857a4
Ycow <skypnal12@gmail.com>
2025-08-11 14:05:55 +0900

    feat: querykey 팩토리, http 추가

---
+++ src/shared/api/http.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosRequestConfig } from "axios"
+
+const axiosInstance = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+export const http = {
+  get: async <Response = unknown>(url: string, options: AxiosRequestConfig = {}) => {
+    const response = await axiosInstance.get<Response>(url, options)
+    return response.data
+  },
+  post: async <Request = unknown, Response = unknown>(url: string, data?: Request, options?: AxiosRequestConfig) => {
+    const response = await axiosInstance.post<Response>(url, data, options)
+    return response.data
+  },
+  put: async <Request = unknown, Response = unknown>(url: string, data?: Request) => {
+    const response = await axiosInstance.put<Response>(url, data)
+    return response.data
+  },
+  del: async <Response = unknown>(url: string) => {
+    const response = await axiosInstance.delete<Response>(url)
+    return response.data
+  },
+  download: async <Response = Blob>(url: string) => {
+    const response = await axiosInstance.get<Response>(url, { responseType: "blob" })
+    return response.data
+  },
+}
---
+++ src/shared/api/index.ts
@@ -0,0 +1,2 @@
+export { http } from "./http"
+export { createQueryKeys } from "./queryKeys"
